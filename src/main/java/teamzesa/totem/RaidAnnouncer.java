25a35c6c99f43093b2fcc471b345006be62e9584
JwonLee <jaxple@gmail.com>
2023-06-13 21:52:14 +0900

    Add Raid Announcer

---
+++ src/main/java/teamzesa/totem/RaidAnnouncer.java
@@ -0,0 +1,44 @@
+package teamzesa.totem;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.raid.RaidTriggerEvent;
+
+public class RaidAnnouncer implements Listener {
+    private Location loc;
+    private double x;
+    private double z;
+
+    @EventHandler
+    public void raidAnnouncer(RaidTriggerEvent e) {
+        this.loc = e.getRaid().getLocation();
+        String activeWorld = worldName(e.getWorld().getName());
+
+        this.x = loc.getX();
+        this.z = loc.getZ();
+
+        System.out.println(e.getRaid().getBadOmenLevel());
+
+        Bukkit.broadcastMessage(
+                String.format(
+                        ChatColor.RED + "%s 월드의 X : %1.0f Y : %1.0f 에서 레이드가 시작됩니다.",
+                        activeWorld, x, z
+                )
+        );
+    }
+
+    public String worldName(String worldName) {
+        switch (worldName) {
+            case "world" :
+                return "야생";
+            case "world_nether" :
+                return "지옥";
+            case "world_the_end" :
+                return "엔더";
+        }
+        return null;
+    }
+}
