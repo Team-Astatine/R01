976971182c05f89459cc38f3f9fc7e3e61a1b822
JwonLee <jaxple@gmail.com>
2023-06-13 20:47:12 +0900

    First Commit

---
+++ src/main/java/teamzesa/r01/R01.java
@@ -0,0 +1,17 @@
+package teamzesa.r01;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class R01 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
