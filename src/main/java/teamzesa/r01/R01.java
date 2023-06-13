e824798c572703ed9f1b68fc9942dfde039cb63f
JwonLee <jaxple@gmail.com>
2023-06-13 21:53:28 +0900

    Remove onDisable method Add Announcer & Command Executor

--- src/main/java/teamzesa/r01/R01.java
+++ src/main/java/teamzesa/r01/R01.java
@@ -1,17 +1,25 @@
 package teamzesa.r01;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import teamzesa.totem.RaidAnnouncer;
+import teamzesa.totem.TotemStacking;
 
-public final class R01 extends JavaPlugin {
+import java.util.Objects;
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+public final class R01 extends JavaPlugin implements Listener {
+    private static PluginManager pm;
 
+    public R01() {
+        pm = getServer().getPluginManager();
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onEnable() {
+        pm.registerEvents(new RaidAnnouncer(),this);
+
+        Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
     }
 }
