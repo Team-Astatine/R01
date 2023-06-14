e9f7b76c36a2766103f764cb58fe1eea10729356
JwonLee <jaxple@gmail.com>
2023-06-15 01:01:53 +0900

    Add Crystal Casting Exception vaild & return void > boolean

--- src/main/java/teamzesa/attackSpeed/EntityNoDamageSet.java
+++ src/main/java/teamzesa/attackSpeed/EntityNoDamageSet.java
@@ -1,5 +1,6 @@
 package teamzesa.attackSpeed;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,9 +9,14 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class EntityNoDamageSet implements Listener {
 
     @EventHandler
-    public void entityHit(EntityDamageByEntityEvent e) {
+    public boolean entityHit(EntityDamageByEntityEvent e) {
+        if (e.getEntity().getType() == EntityType.ENDER_CRYSTAL)
+            return false;
+
         LivingEntity target = (LivingEntity) e.getEntity();
+
         //default 20
         target.setMaximumNoDamageTicks(1);
+        return true;
     }
 }
