04f526c821872d50a78a8bd06c81833751e5bacd
JwonLee <jaxple@gmail.com>
2023-06-14 16:25:33 +0900

    Add event Class

--- src/main/java/teamzesa/r01/R01.java
+++ src/main/java/teamzesa/r01/R01.java
@@ -4,10 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import teamzesa.attackSpeed.EntityNoDamageSet;
+import teamzesa.attackSpeed.PlayerAttackSpeed;
+import teamzesa.expHandler.PlayerExpHealthScale;
 import teamzesa.totem.RaidAnnouncer;
 import teamzesa.totem.TotemStacking;
-
-import java.util.Objects;
 
 public final class R01 extends JavaPlugin implements Listener {
     private static PluginManager pm;
@@ -18,6 +19,10 @@ public final class R01 extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+//        pm.registerEvents(PlayerAttackSpeed.getInstance(),this);
+        pm.registerEvents(new PlayerAttackSpeed(),this);
+        pm.registerEvents(new PlayerExpHealthScale(),this);
+        pm.registerEvents(new EntityNoDamageSet(),this);
         pm.registerEvents(new RaidAnnouncer(),this);
 
         Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
