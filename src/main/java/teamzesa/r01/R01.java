99431c622ef3d0d931518d4bf04409a75d102609
JwonLee <jaxple@gmail.com>
2023-06-14 17:17:44 +0900

    Add threading make instance

--- src/main/java/teamzesa/r01/R01.java
+++ src/main/java/teamzesa/r01/R01.java
@@ -1,30 +1,48 @@
 package teamzesa.r01;
 
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.attackSpeed.EntityNoDamageSet;
-import teamzesa.attackSpeed.PlayerAttackSpeed;
+import teamzesa.expHandler.PlayerDyingSet;
 import teamzesa.expHandler.PlayerExpHealthScale;
 import teamzesa.totem.RaidAnnouncer;
 import teamzesa.totem.TotemStacking;
 
-public final class R01 extends JavaPlugin implements Listener {
+public final class R01 extends JavaPlugin implements Listener,Runnable {
     private static PluginManager pm;
-
+    PlayerExpHealthScale playerExpHealthScale;
     public R01() {
         pm = getServer().getPluginManager();
+        playerExpHealthScale = new PlayerExpHealthScale();
     }
 
     @Override
     public void onEnable() {
-//        pm.registerEvents(PlayerAttackSpeed.getInstance(),this);
-        pm.registerEvents(new PlayerAttackSpeed(),this);
-        pm.registerEvents(new PlayerExpHealthScale(),this);
+        pm.registerEvents(new PlayerDyingSet(),this);
         pm.registerEvents(new EntityNoDamageSet(),this);
         pm.registerEvents(new RaidAnnouncer(),this);
+        pm.registerEvents(this,this);
 
         Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
+        Bukkit.getScheduler().runTaskTimerAsynchronously(
+                this,this,0L,20L);
+    }
+
+    @Override
+    public void run() {
+        pm.registerEvents(new PlayerExpHealthScale(),this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        playerExpHealthScale.expChangeEvent(p);
+        p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(40.0);
     }
 }
