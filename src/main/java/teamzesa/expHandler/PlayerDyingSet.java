1f7ef92429972d1f7dc9ff2e4267c81ed7c8c6d6
JwonLee <jaxple@gmail.com>
2023-06-14 17:17:30 +0900

    Split PlayerDyingSet.java

---
+++ src/main/java/teamzesa/expHandler/PlayerDyingSet.java
@@ -0,0 +1,12 @@
+package teamzesa.expHandler;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDyingSet implements Listener {
+    @EventHandler
+    public void onDeathPlayer(PlayerDeathEvent e) {
+        e.getEntity().setHealthScale(20);
+    }
+}
