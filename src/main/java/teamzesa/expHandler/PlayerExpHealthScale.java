0c117800c255881d2e22930e5254c1b113af02a6
JwonLee <jaxple@gmail.com>
2023-06-15 01:00:09 +0900

    Add threading & Refactor Code

--- src/main/java/teamzesa/expHandler/PlayerExpHealthScale.java
+++ src/main/java/teamzesa/expHandler/PlayerExpHealthScale.java
@@ -1,38 +1,30 @@
 package teamzesa.expHandler;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerExpHealthScale implements Listener {
     private final double MAX_HEALTH_SCALE = 60.0;
     private final int STANDARD_LEVEL = 10;
     private final int DEFAULT_PLAYER_HEALTH = 20;
 
-    private Player p;
-
     @EventHandler
     public void expChangeEvent(PlayerLevelChangeEvent e) {
-        p = e.getPlayer();
-        int changeLevel = p.getLevel();
-        double playerHealthScale = p.getHealthScale();
-
-        if (playerHealthScale > MAX_HEALTH_SCALE)
-            return;
-
-        if (changeLevel % STANDARD_LEVEL != 0)
-            return;
-
-        int extraHealth = (changeLevel / STANDARD_LEVEL) * 2;
-        p.setHealthScale(
-                DEFAULT_PLAYER_HEALTH + (double)extraHealth
-        );
+        Player player = e.getPlayer();
+        updatePlayerHealthScale(player);
     }
 
-    public void expChangeEvent(Player p) {
-        int playerLevel = p.getLevel();
-        double playerHealthScale = p.getHealthScale();
+    public void expChangeEvent(Player player) {
+        updatePlayerHealthScale(player);
+    }
+
+    private void updatePlayerHealthScale(Player player) {
+        int playerLevel = player.getLevel();
+        double playerHealthScale = player.getHealthScale();
 
         if (playerHealthScale > MAX_HEALTH_SCALE)
             return;
@@ -40,9 +32,14 @@ public class PlayerExpHealthScale implements Listener {
         if (playerLevel < STANDARD_LEVEL)
             return;
 
-        int extraHealth = (playerLevel / STANDARD_LEVEL) * 2;
-        p.setHealthScale(
-                DEFAULT_PLAYER_HEALTH + (double)extraHealth
-        );
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                int extraHealth = (playerLevel / STANDARD_LEVEL) * 2;
+                player.setHealthScale(DEFAULT_PLAYER_HEALTH + (double) extraHealth);
+            }
+        };
+
+        task.runTaskLater(Bukkit.getPluginManager().getPlugin("R01"), 1L);
     }
 }
