77720679d0b8475815623035eaf5f9eac178bdc2
JwonLee <jaxple@gmail.com>
2023-06-15 16:22:41 +0900

    Add Explotion Asyn Thread

---
+++ src/main/java/teamzesa/explosive/Explotion.java
@@ -0,0 +1,24 @@
+package teamzesa.explosive;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Explotion implements Listener {
+    @EventHandler
+    public boolean explosive(ExplosionPrimeEvent e) {
+        if (e.getEntity().getType() != EntityType.MINECART_TNT)
+            return false;
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                e.setRadius(20);
+                e.setFire(true);
+            }
+        };
+        return true;
+    }
+}
