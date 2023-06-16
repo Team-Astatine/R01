ea4344fa28e01583796b86856f088eef45fce200
JwonLee <jaxple@gmail.com>
2023-06-16 09:38:13 +0900

    Refactor Code

--- src/main/java/teamzesa/r01/R01.java
+++ src/main/java/teamzesa/r01/R01.java
@@ -2,12 +2,15 @@ package teamzesa.r01;
 
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.EntityType;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.attackSpeed.EntityNoDamageSet;
@@ -16,6 +19,8 @@ import teamzesa.expHandler.PlayerExpHealthScale;
 import teamzesa.explosive.Explotion;
 import teamzesa.totem.RaidAnnouncer;
 import teamzesa.totem.TotemStacking;
+
+import java.util.Map;
 
 public final class R01 extends JavaPlugin implements Listener {
     private static PluginManager pm;
@@ -28,19 +33,44 @@ public final class R01 extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         pm.registerEvents(this,this);
-        pm.registerEvents(new RaidAnnouncer(),this);
-        pm.registerEvents(new EntityNoDamageSet(),this);
         pm.registerEvents(new Explotion(),this);
-        pm.registerEvents(new PlayerExpHealthScale(),this);
+        pm.registerEvents(new RaidAnnouncer(),this);
         pm.registerEvents(new PlayerDyingSet(),this);
+        pm.registerEvents(new EntityNoDamageSet(),this);
+        pm.registerEvents(new PlayerExpHealthScale(),this);
 
         Bukkit.getPluginCommand("totem").setExecutor(new TotemStacking());
     }
-
+    
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         playerExpHealthScale.expChangeEvent(p);
         p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(40.0);
+    }
+
+    @EventHandler
+    public void anvil(InventoryClickEvent e) {
+        if (!(e.getInventory() instanceof AnvilInventory))
+            return;
+
+        HumanEntity humanEntity = e.getViewers().get(0);
+        System.out.println(humanEntity.getName());
+
+//        if (anvilInventory.getItem(0) == null && anvilInventory.getItem(1) == null){}
+//
+//        Map<Enchantment,Integer> leftStuffEnchantOption = anvilInventory.getItem(0).getEnchantments();
+//        Map<Enchantment,Integer> rightStuffEnchantOption = anvilInventory.getItem(1).getEnchantments();
+
+        /*for (Enchantment enchantment : leftStuffEnchantOption.keySet()) {
+            int level = enchantment.getMaxLevel();
+            p.sendMessage(String.valueOf(level));
+        }
+
+        for (Enchantment enchantment : rightStuffEnchantOption.keySet()) {
+            int level = enchantment.getMaxLevel();
+            p.sendMessage(String.valueOf(level));
+        }*/
+
     }
 }
