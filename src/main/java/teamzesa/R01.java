e40f1f8823b8004a346865980d1b63a705f4c1cf
JwonLee <jaxple@gmail.com>
2023-06-23 11:18:44 +0900

    Add JoinEvent Class & IO method

--- src/main/java/teamzesa/R01.java
+++ src/main/java/teamzesa/R01.java
@@ -1,24 +1,32 @@
 package teamzesa;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import teamzesa.combat.EntityDamageTickingHandler;
+import teamzesa.combat.*;
 import teamzesa.command.NameChanger;
-import teamzesa.combat.PlayerDyingHandler;
-import teamzesa.combat.PlayerHealthScaleHandler;
-import teamzesa.combat.ExplosiveHandler;
 import teamzesa.announcer.RaidAnnouncer;
 import teamzesa.command.TotemStacking;
+import teamzesa.user.User;
 import teamzesa.user.UserHandler;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class R01 extends JavaPlugin implements Listener {
     private static PluginManager pm;
+    private final UserHandler userHandler;
+    private final File userDataFile;
 
     public R01() {
         pm = getServer().getPluginManager();
+        userHandler = UserHandler.getInstance();
+        userDataFile = new File(this.getDataFolder(), "userData.json");
     }
 
     private void commandHandler() {
@@ -26,25 +34,39 @@ public final class R01 extends JavaPlugin implements Listener {
         this.getCommand("nameChanger").setExecutor(new NameChanger());
     }
 
-    private void inputUserData() {
-        UserHandler userHandler = UserHandler.getInstance();
-        File userDataFile = new File(this.getDataFolder(),"userData.yml");
-
+    private void inputUserData() throws IOException {
+//        데이터 해쉬맵에 쑤셔넣기
     }
 
     private void outputUserData() {
+        List<User> userData = new ArrayList<>(userHandler.getUserMap().values());
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
 
+        try (FileWriter writer = new FileWriter(userDataFile)) {
+            gson.toJson(userData, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Override
     public void onEnable() {
-        this.commandHandler(); // command set
+        commandHandler(); // command set
 
         pm.registerEvents(this,this);
         pm.registerEvents(new RaidAnnouncer(),this);
+        pm.registerEvents(new UserJoinHandler(),this);
         pm.registerEvents(new ExplosiveHandler(),this);
-        pm.registerEvents(new PlayerDyingHandler(),this);
-        pm.registerEvents(new PlayerHealthScaleHandler(),this);
+        pm.registerEvents(new UserDyingHandler(),this);
+        pm.registerEvents(new UserHealthScaleHandler(),this);
         pm.registerEvents(new EntityDamageTickingHandler(),this);
+    }
+
+    @Override
+    public void onDisable() {
+        outputUserData();
     }
 }
