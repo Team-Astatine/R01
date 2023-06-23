670256d7c850cfdc867685de9a985e7a3321bc2f
JwonLee <jaxple@gmail.com>
2023-06-23 11:55:29 +0900

    Remove Hashmap code

--- src/main/java/teamzesa/combat/UserJoinHandler.java
+++ src/main/java/teamzesa/combat/UserJoinHandler.java
@@ -5,21 +5,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import teamzesa.user.User;
 import teamzesa.user.UserHandler;
-
-import java.util.Map;
-import java.util.UUID;
 
 public class UserJoinHandler implements Listener {
     private UserHealthScaleHandler playerExpHealthScale;
     private UserHandler userHandler;
-    private Map<UUID,User> userData;
 
     public UserJoinHandler() {
         playerExpHealthScale = new UserHealthScaleHandler();
         userHandler = UserHandler.getInstance();
-        userData = userHandler.getUserMap();
     }
 
     @EventHandler
@@ -28,7 +22,7 @@ public class UserJoinHandler implements Listener {
         playerExpHealthScale.expChangeEvent(player);
         player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(40.0);
 
-        if (userData.get(player.getUniqueId()) == null)
+        if (userHandler.getUser(player) == null)
             userHandler.addUser(player);
     }
 }
