21236ab19aebd3ad92e3e21d0f69ad1b1a75fb76
JwonLee <jaxple@gmail.com>
2023-06-23 11:55:10 +0900

    Edit CRUD method

--- src/main/java/teamzesa/user/UserHandler.java
+++ src/main/java/teamzesa/user/UserHandler.java
@@ -24,19 +24,27 @@ public class UserHandler {
     }
 
     public void addUser(Player player) {
-        Bukkit.getLogger().info(player.getName() + "님이 등록됐습니다.");
+        Bukkit.getLogger().info(player.getName() + "님이 신규유저 등록됐습니다.");
         userData.put(player.getUniqueId(), new User(player));
+    }
+
+    public void updateUserData(User[] newUserData) {
+        for (User user : newUserData) {
+            userData.put(user.getUuid(), user);
+//            Bukkit.getLogger().info(user.getName());
+        }
     }
 
     public User getUser(Player player) {
         return  userData.get(player.getUniqueId());
     }
 
-    public void updateUser(Player player) {
-        userData.replace(player.getUniqueId(), new User(player));
+    public void updateUser(User user) {
+        userData.replace(user.getUuid(), user);
     }
 
     public void removeUser(Player player) {
+        Bukkit.getLogger().info(player.getName() + "님이 삭제됐습니다.");
         userData.remove(player.getUniqueId());
     }
 }
