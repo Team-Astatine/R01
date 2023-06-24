6dc2a640fe5f07097f1c48e9f3075098f56d70f0
JwonLee <jaxple@gmail.com>
2023-06-24 11:44:49 +0900

    Remove expChangeEvent Overload method

--- src/main/java/teamzesa/combat/UserHealthScaleHandler.java
+++ src/main/java/teamzesa/combat/UserHealthScaleHandler.java
@@ -6,19 +6,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import teamzesa.user.UserHandler;
 
 public class UserHealthScaleHandler implements Listener {
     private final double MAX_HEALTH_SCALE = 60.0;
     private final int STANDARD_LEVEL = 10;
     private final int DEFAULT_PLAYER_HEALTH = 20;
+    private final UserHandler userHandler;
+
+    public UserHealthScaleHandler() {
+        userHandler = UserHandler.getInstance();
+    }
 
     @EventHandler
     public void expChangeEvent(PlayerLevelChangeEvent e) {
         Player player = e.getPlayer();
-        updatePlayerHealthScale(player);
-    }
-
-    public void expChangeEvent(Player player) {
         updatePlayerHealthScale(player);
     }
 
@@ -37,6 +39,7 @@ public class UserHealthScaleHandler implements Listener {
             public void run() {
                 int extraHealth = (playerLevel / STANDARD_LEVEL) * 2;
                 player.setHealthScale(DEFAULT_PLAYER_HEALTH + (double) extraHealth);
+                userHandler.updateUser(player.getUniqueId(),player.getHealthScale());
             }
         };
 
