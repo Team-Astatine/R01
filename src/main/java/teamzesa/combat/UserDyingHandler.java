8f367b846b40e47fcbe977420bcd7d4c5524d6d5
JwonLee <jaxple@gmail.com>
2023-06-24 13:25:20 +0900

    Refactor UserHandling

--- src/main/java/teamzesa/combat/UserDyingHandler.java
+++ src/main/java/teamzesa/combat/UserDyingHandler.java
@@ -1,12 +1,19 @@
 package teamzesa.combat;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import teamzesa.user.User;
+import teamzesa.user.UserHandler;
 
 public class UserDyingHandler implements Listener {
+    private UserHandler userHandler;
+    public UserDyingHandler() {
+        userHandler = UserHandler.getInstance();
+    }
     @EventHandler
     public void onDeathPlayer(PlayerDeathEvent e) {
-        e.getEntity().setHealthScale(20);
+        userHandler.updateDyingUser(e.getEntity());
     }
 }
