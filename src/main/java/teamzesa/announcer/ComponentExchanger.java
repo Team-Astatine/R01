ec938e7cd5a7838e424ac0c1aa1bb9fa9fd7e702
JwonLee <jaxple@gmail.com>
2023-07-03 18:23:08 +0900

    Update ComponentExchanger

--- src/main/java/teamzesa/announcer/ComponentExchanger.java
+++ src/main/java/teamzesa/announcer/ComponentExchanger.java
@@ -6,27 +6,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class ComponentExchanger {
-
-    private static ComponentExchanger instance;
     private static Component component;
-
-    public static ComponentExchanger getAnnouncerHandler() {
-        if (instance == null)
-            return new ComponentExchanger();
-        return instance;
-    }
 
     public static void serverAnnouncer(String string,Integer color) {
         component = Component.text(string)
                 .color(TextColor.color(color));
-
         Bukkit.broadcast(component);
     }
 
     public static void playerAnnouncer(Player player, String string,TextColor color) {
         component = Component.text(string)
                 .color(TextColor.color(color));
-
         player.sendMessage(component);
     }
 
@@ -38,13 +28,6 @@ public class ComponentExchanger {
     public static Component componentSet(String string,TextColor color) {
         component = Component.text(string)
                 .color(TextColor.color(color));
-        return component;
-    }
-
-    public static Component nameChanger(String prefix, TextColor color) {
-        component = Component.text(prefix)
-                .color(color);
-
         return component;
     }
 }
