079ccb50da38d2c00bee39917a2009c7a2b9f4a1
JwonLee <jaxple@gmail.com>
2023-07-03 15:03:47 +0900

    Add String Component Exchanger

---
+++ src/main/java/teamzesa/announcer/ComponentHandler.java
@@ -0,0 +1,52 @@
+package teamzesa.announcer;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class ComponentHandler {
+
+    private static ComponentHandler instance;
+    private static Component component;
+
+    public static ComponentHandler getAnnouncerHandler() {
+        if (instance == null)
+            return new ComponentHandler();
+        return instance;
+    }
+
+    public static void serverAnnouncer(String string,Integer color) {
+        component = Component.text(string)
+                .color(TextColor.color(color));
+
+        Bukkit.broadcast(component);
+    }
+
+    public static void playerAnnouncer(Player player, String string,TextColor color) {
+        component = Component.text(string)
+                .color(TextColor.color(color));
+
+        player.sendMessage(component);
+    }
+
+    public static Component componentSet(String string) {
+        component = Component.text(string);
+        return component;
+    }
+
+    public static Component componentSet(String string,TextColor color) {
+        component = Component.text(string)
+                .color(TextColor.color(color));
+        return component;
+    }
+
+    public static Component nameChanger(String prefix , String name, TextColor color) {
+        component = Component.text(prefix)
+                .color(color)
+                .append(Component.text(" " + name, NamedTextColor.WHITE));
+
+        return component;
+    }
+}
