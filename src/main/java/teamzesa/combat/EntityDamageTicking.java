5cd22bf8a73f9d580f8ea8c39cafa68249f9ffdd
JwonLee <jaxple@gmail.com>
2023-07-05 17:46:39 +0900

    Refactor Code Add Weapon Option

--- src/main/java/teamzesa/combat/EntityDamageTicking.java
+++ src/main/java/teamzesa/combat/EntityDamageTicking.java
@@ -1,26 +1,50 @@
 package teamzesa.combat;
 
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class EntityDamageTicking implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void entityHit(EntityDamageByEntityEvent e) {
-        if (e.getEntityType() != EntityType.PLAYER)
+        if (!e.getDamager().getType().equals(EntityType.PLAYER))
             return;
 
-        if (e.getDamager().getType() != EntityType.PLAYER)
+        if (!e.getEntityType().equals(EntityType.PLAYER))
             return;
 
+        Player heater = (Player) e.getDamager();
         Player target = (Player) e.getEntity();
+
         //default 20
-        target.setMaximumNoDamageTicks(1);
+        int hurtTick = 20;
+        boolean stuffCheck = handStuffChecker(
+                heater.getInventory().getItemInMainHand() ,
+                heater.getInventory().getItemInOffHand());
+
+        if (!stuffCheck) //One Hand Sword
+            hurtTick = 10;
+
+        if (stuffCheck) //Two Hand Sword
+            hurtTick = 1;
+
+        target.setMaximumNoDamageTicks(hurtTick);
 //        ((LivingEntity) e.getEntity()).setMaximumNoDamageTicks(1);
+    }
+
+    private Boolean handStuffChecker(ItemStack mainHand , ItemStack offHand) {
+        if (mainHand.getType().equals(Material.NETHERITE_SWORD) && offHand.getType().equals(Material.NETHERITE_SWORD))
+            return true;
+
+        if (mainHand.getType().equals(Material.NETHERITE_AXE) && offHand.getType().equals(Material.NETHERITE_AXE))
+            return true;
+
+        return false;
     }
 }
