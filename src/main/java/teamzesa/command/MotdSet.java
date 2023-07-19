94c846a45882d8c73402e4667fb5c8f26ef13711
JwonLee <jaxple@gmail.com>
2023-07-19 16:34:47 +0900

    Edit Announcer Message & configIO Saving

--- src/main/java/teamzesa/command/MotdSet.java
+++ src/main/java/teamzesa/command/MotdSet.java
@@ -1,25 +1,36 @@
 package teamzesa.command;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import teamzesa.ComponentExchanger;
 
 public class MotdSet implements CommandExecutor {
+    private FileConfiguration config;
+
+    public MotdSet(FileConfiguration config) {
+        this.config = config;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player)sender;
-
         if (!player.getName().equals("JAXPLE")) {
-            ComponentExchanger
-                    .componentSet("해당 명령어는 플레이어가 사용할 수 없습니다.", "RED");
+            ComponentExchanger.componentSet("해당 명령어는 플레이어가 사용할 수 없습니다.", "RED");
             return false;
         }
 
-        Bukkit.motd(ComponentExchanger.componentSet(args[0]));
-        Bukkit.getLogger().info(args[0] + "로 MOTD 재설정");
+        StringBuilder customMotd = new StringBuilder();
+        if (args.length > 1) {
+            for (String motd : args)
+                customMotd.append(motd);
+        }
+
+        config.set("motd",customMotd);
+        player.sendMessage(
+                ComponentExchanger.componentSet(customMotd.append(" 로 변경 됐습니다."),"YELLOW"));
         return true;
     }
 }
