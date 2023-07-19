a71b551558a56cbcf45dd2c3a1e740c2cad3c060
JwonLee <jaxple@gmail.com>
2023-07-19 18:44:26 +0900

    Update ConfigIOHandler instance

--- src/main/java/teamzesa/R01.java
+++ src/main/java/teamzesa/R01.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.combat.*;
 import teamzesa.command.*;
+import teamzesa.configIOHandler.ConfigIOHandler;
 import teamzesa.worldSet.RaidAnnouncer;
 import teamzesa.userValue.UserIOHandler;
 import teamzesa.userValue.JoinAndQuit;
@@ -13,10 +14,12 @@ import java.io.File;
 public final class R01 extends JavaPlugin {
     private static PluginManager pm;
     private static UserIOHandler userIoHandler;
+    private static ConfigIOHandler configIOHandler;
 
     public R01() {
         pm = getServer().getPluginManager();
         userIoHandler = UserIOHandler.getIOHandler();
+        configIOHandler = ConfigIOHandler.getConfigIOHandler(new File(getDataFolder(),"config.yml"));
     }
 
     private File checkUpDataFile() {
@@ -26,13 +29,13 @@ public final class R01 extends JavaPlugin {
     private void commandHandler() {
         this.getCommand("운석").setExecutor(new Meteor());
         this.getCommand("나").setExecutor(new VoChecker());
+        this.getCommand("Motd").setExecutor(new MotdSet());
         this.getCommand("머리").setExecutor(new ArmourSet());
         this.getCommand("몸통").setExecutor(new ArmourSet());
         this.getCommand("바지").setExecutor(new ArmourSet());
         this.getCommand("신발").setExecutor(new ArmourSet());
         this.getCommand("토템").setExecutor(new TotemStacking());
         this.getCommand("체력초기화").setExecutor(new HealthSet());
-        this.getCommand("Motd").setExecutor(new MotdSet(getConfig()));
         this.getCommand("SaveUserData").setExecutor(new SaveUserData(checkUpDataFile()));
     }
 
@@ -50,6 +53,7 @@ public final class R01 extends JavaPlugin {
         this.commandHandler(); // command set
         this.functionHandler(); // function set
         this.saveDefaultConfig(); // dataFile set
+//        configIOHandler.getMotd(); // motd set
         userIoHandler.inputUserData(checkUpDataFile()); // userData Set
     }
 
