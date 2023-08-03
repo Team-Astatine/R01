c5bd6f6a0af574d42414cf7a7a05d3759ebf7dc4
JwonLee <jaxple@gmail.com>
2023-08-03 22:07:51 +0900

    Add Threading Swing Hand

--- src/main/java/teamzesa/combat/HandSwing.java
+++ src/main/java/teamzesa/combat/HandSwing.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.scheduler.BukkitRunnable;
 
 
 public class HandSwing implements Listener {
@@ -13,8 +14,15 @@ public class HandSwing implements Listener {
     public void onSwing(PlayerArmSwingEvent e) {
         if (e.getHand() == EquipmentSlot.OFF_HAND)
             e.setCancelled(true);
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
 
-        Player player = (Player) e.getPlayer();
-        player.swingOffHand();
+                Player player = (Player) e.getPlayer();
+                player.swingOffHand();
+
+            }
+        };
+        task.run();
     }
 }
