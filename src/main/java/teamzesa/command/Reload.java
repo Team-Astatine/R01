8b4c28bdcaa1a193971da83687f82251c219cb1e
JwonLee <jaxple@gmail.com>
2023-08-05 13:09:10 +0900

    Add Player.sendMessage

--- src/main/java/teamzesa/command/Reload.java
+++ src/main/java/teamzesa/command/Reload.java
@@ -24,7 +24,7 @@ public class Reload extends ComponentExchanger implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         Player player = (Player)sender;
         if (!player.getName().equals("JAXPLE")) {
-            componentSet("해당 명령어는 플레이어가 사용할 수 없습니다.", Color.RED);
+            player.sendMessage(componentSet("해당 명령어는 플레이어가 사용할 수 없습니다.", Color.RED));
             return false;
         }
 
