af4138253e2b0cc44ad868a894e339d832985049
JwonLee <jaxple@gmail.com>
2023-08-05 13:14:47 +0900

    Add Ghast Cancel dying

--- src/main/java/teamzesa/combat/Explosive.java
+++ src/main/java/teamzesa/combat/Explosive.java
@@ -1,9 +1,12 @@
 package teamzesa.combat;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreeperPowerEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 
@@ -15,7 +18,13 @@ public class Explosive implements Listener {
     }
 
     @EventHandler
-    public void creeperSet(CreeperPowerEvent e) {
+    public void creeperSet(CreeperPowerEvent e) {}
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void entityDeath(EntityExplodeEvent e) {
+        if (!e.getEntityType().equals(EntityType.GHAST))
+            return;
+
+        e.setCancelled(true);
     }
 }
