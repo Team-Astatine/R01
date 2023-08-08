79e91229fc088ebbb1b034f244ecdd396dcffe56
JwonLEE <jaxple@gmail.com>
2023-08-08 16:29:13 +0900

    Add Something

--- src/main/java/teamzesa/combat/Explosive.java
+++ src/main/java/teamzesa/combat/Explosive.java
@@ -4,43 +4,41 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreeperPowerEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
-import org.bukkit.scheduler.BukkitRunnable;
-
 
 public class Explosive implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void explosiveMineCart(ExplosionPrimeEvent e) {
         switch (e.getEntityType()) {
-            case PRIMED_TNT -> {boomBer();  break;}
-            case CREEPER -> {creeperBoom(); break;}
-            case GHAST -> {ghastBoom(); break;}
-            case WITHER_SKULL -> {witherBoom(); break;}
-            case MINECART_TNT -> {cartBoom();   break;}
+            case PRIMED_TNT -> boomBer(e);
+            case CREEPER -> creeperBoom(e);
+            case GHAST -> ghastBoom(e);
+            case WITHER_SKULL -> witherBoom(e);
+            case MINECART_TNT -> cartBoom(e);
             default -> e.setCancelled(true);
         }
     }
 
-    private void boomBer() {
+    private void boomBer(ExplosionPrimeEvent e) {
+        e.setCancelled(true);
+        e.getEntity()
+                .getWorld()
+                .createExplosion(e.getEntity().getLocation(), e.getRadius());
+    }
+
+    private void creeperBoom(ExplosionPrimeEvent e) {
 
     }
 
-    private void creeperBoom() {
+    private void ghastBoom(ExplosionPrimeEvent e) {
 
     }
 
-    private void ghastBoom() {
+    private void witherBoom(ExplosionPrimeEvent e) {
 
     }
 
-    private void witherBoom() {
-
-    }
-
-    private void cartBoom() {
+    private void cartBoom(ExplosionPrimeEvent e) {
 
     }
 }
