f9ec8ed9df4e80a14bc1e427307a4c01e03cd661
JwonLEE <jaxple@gmail.com>
2023-08-11 16:49:47 +0900

    Refactor world Config

--- src/main/java/teamzesa/command/Reload.java
+++ src/main/java/teamzesa/command/Reload.java
@@ -29,7 +29,7 @@ public class Reload extends ComponentExchanger implements CommandExecutor {
         }
 
         configIOHandler.fileLoader(dataPathFile);
-        configIOHandler.worldConfigLoad();
+        configIOHandler.getWorldConfig();
         configIOHandler.getMineListConfig();
         configIOHandler.getDiscordConfig();
         configIOHandler.getMineListConfig();
