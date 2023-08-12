ccb5d31b56dda7a1a210f9a697c5bb16b1cdfe20
JwonLee <jaxple@gmail.com>
2023-08-12 13:29:10 +0900

    Refactor Announcer Add joinAnnouncer method

--- src/main/java/teamzesa/worldSet/Announcer.java
+++ src/main/java/teamzesa/worldSet/Announcer.java
@@ -1,10 +1,10 @@
 package teamzesa.worldSet;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import teamzesa.ComponentExchanger;
 import teamzesa.IOHandler.ConfigIOHandler;
@@ -16,6 +16,7 @@ public class Announcer extends ComponentExchanger {
     private static class AnnouncerHolder {
         private static final Announcer INSTANCE = new Announcer();
     }
+
     private final ConfigIOHandler configIOHandler;
 
     private Announcer() {
@@ -26,26 +27,35 @@ public class Announcer extends ComponentExchanger {
         return AnnouncerHolder.INSTANCE;
     }
 
+    public void joinAnnouncer(Player player) {
+        Component[] components = createComponents("================[환영합니다]================");
+        for (Component component : components)
+            player.sendMessage(component);
+    }
+
     public void defaultAnnouncer(JavaPlugin plugin) {
         int delay = 0; // 초기 딜레이 (0 틱, 즉 즉시 시작)
         int interval = 18000; // 3분마다 (1초 = 20틱)
 
+        Component[] components = createComponents("================[클릭하세요]================");
+
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            for (Component component : components)
+                Bukkit.broadcast(component);
+        }, delay, interval);
+    }
+
+    private Component[] createComponents(String title) {
         String mineListLink = configIOHandler.getMineListConfig();
         String discordLink = configIOHandler.getDiscordConfig();
         String notionLink = configIOHandler.getNotionConfig();
 
-        Component firstLine = componentSet("================[클릭하세요]================");
-        Component secondLine = componentSet("========================================");
-        Component mineList = createLinkComponent("[클릭] > 마인리스트 추천",mineListLink,Color.GREEN);
-        Component discord = createLinkComponent("[클릭] > 디스코드 참가",discordLink,new Color(114, 137, 218));
-        Component notion = createLinkComponent("[클릭] > 서버 플레이 가이드",notionLink,new Color(78, 115, 223));
+        Component firstLine = componentSet(title, new Color(221, 255, 170));
+        Component mineList = createLinkComponent("[클릭] > 마인리스트 추천", mineListLink, new Color(167, 123, 202));
+        Component discord = createLinkComponent("[클릭] > 디스코드 참가", discordLink, new Color(114, 137, 218));
+        Component notion = createLinkComponent("[클릭] > 서버 플레이 가이드", notionLink, new Color(112, 71, 157));
+        Component secondLine = componentSet("========================================", new Color(221, 255, 170));
 
-        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-            Bukkit.broadcast(firstLine);
-            Bukkit.broadcast(mineList);
-            Bukkit.broadcast(discord);
-            Bukkit.broadcast(notion);
-            Bukkit.broadcast(secondLine);
-        }, delay, interval);
+        return new Component[]{firstLine, mineList, discord, notion, secondLine};
     }
 }
