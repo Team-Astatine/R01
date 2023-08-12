7cae918c20deb2f0c2221d9ea2f5d03dce495a66
JwonLee <jaxple@gmail.com>
2023-08-12 13:17:13 +0900

    Add player send message

--- src/main/java/teamzesa/command/VoChecker.java
+++ src/main/java/teamzesa/command/VoChecker.java
@@ -4,18 +4,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import teamzesa.ComponentExchanger;
 import teamzesa.userValue.User;
 import teamzesa.userValue.UserHandler;
 
-public class VoChecker implements CommandExecutor {
+import java.awt.*;
+
+public class VoChecker extends ComponentExchanger implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         User user = UserHandler
                 .getUserHandler()
                 .getUser(Bukkit.getPlayer(args[0]));
 
-        Bukkit.getLogger().info(user.toString());
+        playerAnnouncer((Player) sender,user.toString(), Color.YELLOW);
         return true;
     }
 }
