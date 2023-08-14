6687af1f89620a90150727ba4055cca22b5dd2f1
JwonLEE <jaxple@gmail.com>
2023-08-14 11:54:40 +0900

    Edit HandSwing.java Remove Thread

--- src/main/java/teamzesa/combat/HandSwing.java
+++ src/main/java/teamzesa/combat/HandSwing.java
@@ -6,7 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.scheduler.BukkitRunnable;
 
 
 public class HandSwing implements Listener {
@@ -14,15 +13,8 @@ public class HandSwing implements Listener {
     public void onSwing(PlayerArmSwingEvent e) {
         if (e.getHand() == EquipmentSlot.OFF_HAND)
             e.setCancelled(true);
-        BukkitRunnable task = new BukkitRunnable() {
-            @Override
-            public void run() {
 
-                Player player = (Player) e.getPlayer();
-                player.swingOffHand();
-
-            }
-        };
-        task.run();
+        Player player = e.getPlayer();
+        player.swingOffHand();
     }
 }
