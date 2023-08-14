36272334c358ae0e1cc37882a0ccd4e31ac6004c
JwonLee <jaxple@gmail.com>
2023-08-14 22:46:16 +0900

    Update Swing OffHand Better than after

--- src/main/java/teamzesa/combat/HandSwing.java
+++ src/main/java/teamzesa/combat/HandSwing.java
@@ -6,15 +6,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import teamzesa.ComponentExchanger;
+import teamzesa.R01;
 
 
-public class HandSwing implements Listener {
+public class HandSwing extends ComponentExchanger implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onSwing(PlayerArmSwingEvent e) {
         if (e.getHand() == EquipmentSlot.OFF_HAND)
-            e.setCancelled(true);
+            return;
 
-        Player player = e.getPlayer();
-        player.swingOffHand();
+        BukkitTask offHandSwingTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                e.getPlayer().swingOffHand();
+            }
+        }.runTaskLater(R01.getPlugin(R01.class), 7L);
     }
 }
