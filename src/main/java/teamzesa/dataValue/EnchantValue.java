577459298046e30600907e89b4deabdb4c30ec4f
JwonLEE <jaxple@gmail.com>
2023-08-23 11:29:27 +0900

    Override HashCode , equals

--- src/main/java/teamzesa/dataValue/EnchantValue.java
+++ src/main/java/teamzesa/dataValue/EnchantValue.java
@@ -2,6 +2,8 @@ package teamzesa.dataValue;
 
 import org.bukkit.enchantments.Enchantment;
 
+import java.util.Objects;
+
 public class EnchantValue {
     private Enchantment enchantment;
     private int enchantLevel;
@@ -20,10 +22,22 @@ public class EnchantValue {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EnchantValue that)) return false;
+        return getEnchantLevel() == that.getEnchantLevel() && Objects.equals(getEnchantment(), that.getEnchantment());
+    }
+
+    @Override
     public String toString() {
         return "DataValue{" +
                 "enchantment=" + enchantment +
                 ", enchantLevel=" + enchantLevel +
                 '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEnchantment(), getEnchantLevel());
     }
 }
