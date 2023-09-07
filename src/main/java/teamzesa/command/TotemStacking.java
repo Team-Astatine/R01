1bdbba99ba22f95b03111732dc0c98074dbecdb2
JwonLee <jaxple@gmail.com>
2023-09-07 22:17:14 +0900

    Update Batter Totem Stacking

--- src/main/java/teamzesa/command/TotemStacking.java
+++ src/main/java/teamzesa/command/TotemStacking.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -37,22 +38,24 @@ public class TotemStacking extends ComponentExchanger implements CommandExecutor
 
 //        System.out.println(totemCountData);
 
-//        validation
-        if (validMinimumTotemCount(totemCountData)) {
-            playerAnnouncer(player,"2개 이상의 토템을 가지고 있으셔야 합니다.", Color.RED);
-            return false;
-        }
 
         if (validTotemCommand(totemCountData)) {
             playerAnnouncer(player,"합칠 토템이 없습니다.", Color.RED);
             return false;
         }
 
-//        offHandCheck
+//        validation
+        if (validMinimumTotemCount(totemCountData)) {
+            playerAnnouncer(player,"2개 이상의 토템을 가지고 있으셔야 합니다.", Color.RED);
+            return false;
+        }
+
+//        offHandCheck 토템제공 후 addItem 예정
         ItemStack tempOffHandStuff = player.getInventory().getItemInOffHand();
-        if (tempOffHandStuff.getType() != Material.TOTEM_OF_UNDYING)
-            player.getInventory().addItem(tempOffHandStuff);
-        player.getInventory().setItemInOffHand(null);
+        if (tempOffHandStuff.getType() == Material.TOTEM_OF_UNDYING) {
+            player.getInventory().setItemInOffHand(null);
+            tempOffHandStuff = null;
+        }
 
 //        remove Inventory
         playerItemStack.stream()
@@ -77,6 +80,7 @@ public class TotemStacking extends ComponentExchanger implements CommandExecutor
             player.getInventory().addItem(new ItemStack(Material.TOTEM_OF_UNDYING, totalAmount - STACK));
         }
 
+        player.getInventory().addItem(tempOffHandStuff);
         playerAnnouncer(player,"토템을 합쳤습니다.", Color.YELLOW);
         return true;
     }
