71d1c0dfa83990759a510bd64737b2e5fe26a7a8
jaxple <jaxple@gmail.com>
2023-10-29 00:42:56 +0900

    Refactor method

--- src/main/java/teamzesa/command/HealthSet.java
+++ src/main/java/teamzesa/command/HealthSet.java
@@ -14,22 +14,31 @@ import teamzesa.dataValue.userData.UserMapHandler;
 
 import java.awt.*;
 
-public class HealthSet extends ComponentExchanger implements CommandExecutor {
+public class HealthSet implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String @NotNull [] args) {
-        PotionEffect healEffect = new PotionEffect(PotionEffectType.HEAL, 40, 1);
-        UserMapHandler userMapHandler = UserMapHandler.getUserHandler();
-        Player player = Bukkit.getPlayer(args[0]);
-
-        if (player == null)
+        Player targetPlayer = Bukkit.getPlayer(args[0]);
+        double targetPlayerHealth = Double.parseDouble(args[1]);
+        
+        if (targetPlayer == null) {
             return false;
+        }
 
-        player.addPotionEffect(healEffect);
-        player.setHealthScale(Double.parseDouble(args[1]));
-        player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(Double.parseDouble(args[1]));
-        userMapHandler.updateUser(player.getUniqueId(), player.getHealthScale());
+        setPlayerHealth(targetPlayerHealth, targetPlayer);
+        updatePlayerInfo(targetPlayer);
 
-        ComponentExchanger.playerAnnouncer(player,args[0] + "님의 체력이" + args[1] + "으로 설정됐습니다.", Color.YELLOW);
         return true;
+    }
+
+    private void updatePlayerInfo(Player targetPlayer) {
+        UserMapHandler userMapHandler = UserMapHandler.getUserHandler();
+        userMapHandler.updateUser(targetPlayer.getUniqueId(), targetPlayer.getHealthScale());
+    }
+
+    private void setPlayerHealth(double targetPlayerHealth, Player targetPlayer) {
+        targetPlayer.setHealthScale(targetPlayerHealth);
+        targetPlayer.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 40, 1));
+        targetPlayer.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(targetPlayerHealth);
+        ComponentExchanger.playerAnnouncer(targetPlayer,targetPlayer.getName() + "님의 체력이" + targetPlayerHealth + "으로 설정됐습니다.", Color.YELLOW);
     }
 }
