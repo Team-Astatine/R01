db2a445d7ab3ae2461cefe9187b7d56c49511d74
JwonLEE <jaxple@gmail.com>
2023-10-30 15:15:41 +0900

    Refactor getPlayer & checkup Mod Add ModPlayer Name List

--- src/main/java/teamzesa/command/Moderator.java
+++ src/main/java/teamzesa/command/Moderator.java
@@ -8,18 +8,38 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import teamzesa.resgisterEvent.EventExecutor;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 public class Moderator implements CommandExecutor, EventExecutor {
+    private Set<String> moderatorName;
+
+    public Moderator() {
+        this.moderatorName = new HashSet<>();
+        moderList();
+    }
+
+    private void moderList() {
+        moderatorName.add("JAXPLE");
+        moderatorName.add("18_70015401");
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         if (args.length == 0)
             return false;
 
-        if (!(sender.getName().equals("JAXPLE") || sender.getName().equals("18_70015401")))
+        if (checkupMod(sender))
             return false;
 
-        Player player = Bukkit.getPlayer(args[0]);
-        player.setOp(true);
+        Optional<Player> targetPlayer = Optional.ofNullable(Bukkit.getPlayer(args[0]));
+        targetPlayer.ifPresent(player -> player.setOp(true));
 
         return true;
+    }
+
+    private boolean checkupMod(CommandSender sender) {
+        return moderatorName.stream().noneMatch(name -> sender.getName().equals(name));
     }
 }
