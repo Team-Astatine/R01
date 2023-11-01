78316a6511a12c1ddb96768e03ea32feb69c706f
JwonLEE <jaxple@gmail.com>
2023-11-01 14:17:28 +0900

    Update command korean to english

--- src/main/java/teamzesa/resgisterEvent/CommandListup.java
+++ src/main/java/teamzesa/resgisterEvent/CommandListup.java
@@ -6,19 +6,19 @@ import teamzesa.command.*;
 
 public enum CommandListup {
     FLY("fly", new Fly()),
-    METEOR("운석", new Meteor()),
+    METEOR("meteor", new Meteor()),
     MOTD("Motd", new MotdSet()),
-    ARMOUR_HEAD("머리", new ArmourSet()),
+    ARMOUR_HEAD("hat", new ArmourSet()),
     ARMOUR_CHEST("몸통", new ArmourSet()),
     ARMOUR_LEGS("바지", new ArmourSet()),
     ARMOUR_FEET("신발", new ArmourSet()),
     GOD_MODE("god", new GodModeSet()),
-    MODERATOR("관리자", new Moderator()),
-    TOTEM_STACKING("토템", new TotemStacking()),
-    HEALTH_RESET("체력초기화", new HealthSet()),
-    USER_OBJECT_CHECKER("나", new UserObjectChecker()),
+    MODERATOR("moderator", new Moderator()),
+    TOTEM_STACKING("totem", new TotemStacking()),
+    HEALTH_RESET("setHealth", new HealthSet()),
+    USER_OBJECT_CHECKER("vo", new UserObjectChecker()),
     SAVE_USER_DATA("SaveUserData", new SaveUserData()),
-    CONFIG_RELOAD("dataFileRelaod", new Reload());
+    CONFIG_RELOAD("dataFileReload", new Reload());
 
     private final String command;
     private final CommandExecutor executor;
