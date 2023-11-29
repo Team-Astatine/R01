a215ef142ba961ab16d65ba500e338ee63a1418c
JwonLEE <jaxple@gmail.com>
2023-11-29 15:05:18 +0900

    Edit serviceOff -> allServiceOff Add Comment

--- src/main/java/teamzesa/util/ThreadPool.java
+++ src/main/java/teamzesa/util/ThreadPool.java
@@ -1,36 +1,38 @@
 package teamzesa.util;
 
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class ThreadPool {
-    private static final int CORE_POOL_SIZE = 5;
-    private static final int MAXIMUM_POOL_SIZE = 5;
-    private static final int KEEP_ALIVE_TIME = 10;
-    private static final TimeUnit TIME_UNIT = TimeUnit.MILLISECONDS;
-    private static final int QUEUE_CAPACITY = 10;
-    private final ExecutorService executorService;
-    private final ScheduledExecutorService scheduledExecutorService;
-
     private static class ThreadPoolHolder {
         private static final ThreadPool INSTANCE = new ThreadPool();
     }
+
+    private final ExecutorService executorService;
+    private final ScheduledExecutorService scheduledExecutorService;
 
     public static ThreadPool getThreadPool() {
         return ThreadPoolHolder.INSTANCE;
     }
 
     private ThreadPool() {
+        /*
 //        https://engineerinsight.tistory.com/197
 //        executorService = Executors.newFixedThreadPool(4);
         executorService =  new ThreadPoolExecutor(
-                1, //기본 thread core 1개 유지
-                Integer.MAX_VALUE,  // 최대 쓰레딩 wait 갯수 21억 ㅋ
-                10L, TimeUnit.SECONDS,
-                new SynchronousQueue<>());
-
-        scheduledExecutorService = Executors.newScheduledThreadPool(5);
+                CORE_POOL_SIZE, //기본 thread core 1개 유지
+                MAX_CORE_POOL_SIZE,  // 최대 쓰레딩 wait 갯수 4
+                KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(QUEUE_CAPACITY)
+//                new SynchronousQueue<>()
+        );
+        */
+        executorService = Executors.newFixedThreadPool(4);
+        scheduledExecutorService = Executors.newScheduledThreadPool(4);
     }
 
     public void addTask(Runnable task) {
