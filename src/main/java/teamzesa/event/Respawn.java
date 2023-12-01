4e4daf68bd455a2c525287d49f6dda3b68dfa1a8
jaxple <jaxple@gmail.com>
2023-12-01 22:21:52 +0900

    Fix Die -> only Respawn main world

--- src/main/java/teamzesa/event/Respawn.java
+++ src/main/java/teamzesa/event/Respawn.java
@@ -1,6 +1,8 @@
 package teamzesa.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import teamzesa.util.ComponentExchanger;
 import teamzesa.util.Enum.ColorList;
+import teamzesa.util.Enum.WorldName;
 import teamzesa.util.RanNumGenerator;
 
 
@@ -32,7 +35,7 @@ public class Respawn extends ComponentExchanger implements Listener {
         int z = RanNumGenerator.numGenerator();
         int y = RanNumGenerator.groundChecker(player.getWorld());
 
-        this.event.setRespawnLocation(new Location(player.getWorld(),x,y,z));
+        this.event.setRespawnLocation(new Location(Bukkit.getWorld(WorldName.world.getKoreanWorldName()),x,y,z));
         playerSendMsgComponentExchanger(player,"침대가 없어 랜덤 텔레포트 되었습니다.", ColorList.ORANGE);
     }
 }
