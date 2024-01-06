a4e03afff8793dc551e098b70baa4f32cdfa57fb
jaxple <jaxple@gmail.com>
2024-01-06 13:35:02 +0900

    Update Ref method > Synchronized Monitor method

--- src/main/java/teamzesa/util/userHandler/UserDataBase.java
+++ src/main/java/teamzesa/util/userHandler/UserDataBase.java
@@ -19,25 +19,25 @@ public class UserDataBase extends ComponentExchanger {
         return UserMapHandlerHolder.INSTANCE;
     }
 
-    public ConcurrentHashMap<UUID,User> getAllUserTable() {
+    public synchronized ConcurrentHashMap<UUID,User> getAllUserTable() {
         return USER_DATA;
     }
 
-    public boolean insert(User user) {
+    public synchronized boolean insert(User user) {
         USER_DATA.put(user.uuid(), user);
         return Boolean.TRUE;
     }
 
-    public User select(UUID uuid) {
+    public synchronized User select(UUID uuid) {
         return USER_DATA.get(uuid);
     }
 
-    public Boolean update(User user) {
+    public synchronized Boolean update(User user) {
         USER_DATA.replace(user.uuid(), user);
         return Boolean.TRUE;
     }
 
-    public Boolean clear() {
+    public synchronized Boolean clear() {
         USER_DATA.clear();
         return Boolean.TRUE;
     }
