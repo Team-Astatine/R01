7f22518f4e44436950921e4e1918f4187d08bf83
JwonLEE <jaxple@gmail.com>
2024-02-06 20:20:28 +0900

    Edit Format

--- src/main/java/teamzesa/util/Enum/ColorList.java
+++ src/main/java/teamzesa/util/Enum/ColorList.java
@@ -2,14 +2,14 @@ package teamzesa.util.Enum;
 
 import net.kyori.adventure.text.format.TextColor;
 
-public enum ColorList{
+public enum ColorList {
     RED(TextColor.color(233, 30, 100)),
-    PINK(TextColor.color(255,182,193)),
+    PINK(TextColor.color(255, 182, 193)),
     ORANGE(TextColor.color(255, 142, 0)),
     YELLOW(TextColor.color(255, 228, 0)),
     PURPLE(TextColor.color(139, 0, 255)),
     GREEN(TextColor.color(47, 255, 0)),
-    SKY_BLUE(TextColor.color(173,216,230)),
+    SKY_BLUE(TextColor.color(173, 216, 230)),
     VOTE_COLOR(TextColor.color(167, 123, 202)),
     COMMAND_COLOR(TextColor.color(139, 195, 74)),
     NOTIOIN_COLOR(TextColor.color(112, 71, 157)),
