695bdef5c34c4c7f58136345649d0cf4c957f05e
JwonLEE <jaxple@gmail.com>
2024-02-08 11:56:38 +0900

    Edit Change priority

--- src/main/java/teamzesa/event/EventSection.java
+++ src/main/java/teamzesa/event/EventSection.java
@@ -7,10 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.*;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.event.raid.RaidTriggerEvent;
 import org.jetbrains.annotations.NotNull;
 import org.purpurmc.purpur.event.inventory.AnvilUpdateResultEvent;
@@ -27,48 +24,13 @@ public class EventSection implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
+    public void raidEvent(@NotNull RaidTriggerEvent event) {
+        new RaidAnnouncerEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void deathEvent(@NotNull PlayerDeathEvent event) {
         new DeathEvent(event);
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void respawnEvent(@NotNull PlayerRespawnEvent event) {
-        new RespawnEvent(event);
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void anvilResult(@NotNull AnvilUpdateResultEvent event) {
-        new AnvilResultEvent(event);
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void entityToEntityDamageEvent(@NotNull EntityDamageByEntityEvent event) {
-        new EntityDamageByEntity(event);
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void explosiveEvent(@NotNull EntityExplodeEvent event) {
-        new ExplosiveEvent(event);
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void armSwingEvent(@NotNull PlayerArmSwingEvent event) {
-        new HandSwing(event);
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void playerSwapStuffEvent(@NotNull PlayerSwapHandItemsEvent event) {
-
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void menuClickEvent(@NotNull InventoryClickEvent event) {
-
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void raidEvent(@NotNull RaidTriggerEvent event) {
-        new RaidAnnouncer(event);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -79,6 +41,47 @@ public class EventSection implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void weaponThrowEvent(ProjectileLaunchEvent event) {
         new GodModeTridentThrowEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void respawnEvent(@NotNull PlayerRespawnEvent event) {
+        new RespawnEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void invClickEvent(@NotNull InventoryClickEvent event) {
+
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void explosiveEvent(@NotNull EntityExplodeEvent event) {
+        new ExplosiveEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void armSwingEvent(@NotNull PlayerArmSwingEvent event) {
+        new HandSwingEvent(event);
+    }
+
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void anvilResult(@NotNull AnvilUpdateResultEvent event) {
+        new AnvilResultEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void playerInteractEvent(@NotNull PlayerInteractEvent event) {
+        new EnhanceStuffEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void playerSwapStuffEvent(@NotNull PlayerSwapHandItemsEvent event) {
+
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void entityToEntityDamageEvent(@NotNull EntityDamageByEntityEvent event) {
+        new EntityDmgByEntityEvent(event);
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
