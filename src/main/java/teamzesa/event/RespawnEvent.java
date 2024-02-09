a7467daa1f58aa4e9e5b1005954d22dbe0a3ec9e
jaxple <jaxple@gmail.com>
2024-02-09 20:48:57 +0900

    Edit deprecation method getBedSpawnLocation -> getPotentialBedLocation

--- src/main/java/teamzesa/event/RespawnEvent.java
+++ src/main/java/teamzesa/event/RespawnEvent.java
@@ -8,6 +8,7 @@ import teamzesa.util.Enum.ColorList;
 
 
 public class RespawnEvent extends ComponentExchanger implements EventRegister {
+    private Player player;
     private final PlayerRespawnEvent event;
 
     public RespawnEvent(PlayerRespawnEvent event) {
@@ -18,7 +19,7 @@ public class RespawnEvent extends ComponentExchanger implements EventRegister {
 
     @Override
     public void init() {
-
+        this.player = this.event.getPlayer();
     }
 
     @Override
@@ -27,10 +28,8 @@ public class RespawnEvent extends ComponentExchanger implements EventRegister {
     }
 
     public void deathRandomTeleport() {
-        Player player = this.event.getPlayer();
-
-        if (player.getBedSpawnLocation() == null)
-            playerSendMsgComponentExchanger(player, "침대가 없어 랜덤 텔레포트 되었습니다.", ColorList.ORANGE);
-        else playerSendMsgComponentExchanger(player, "침대로 텔레포트 됩니다.", ColorList.YELLOW);
+        if (this.player.getPotentialBedLocation() == null)
+            playerSendMsgComponentExchanger(this.player, "침대가 없어 랜덤 텔레포트 되었습니다.", ColorList.ORANGE);
+        else playerSendMsgComponentExchanger(this.player, "침대로 텔레포트 됩니다.", ColorList.YELLOW);
     }
 }
