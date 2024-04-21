a956a920b86e3c33028b8a7afed07a34059a523a
JAXPLE <jaxple@gmail.com>
2024-04-21 12:56:32 +0900

    Weapon reinforcement implementation completed

--- src/main/java/teamzesa/event/Enhance/EnhanceItemDmgEvent.java
+++ src/main/java/teamzesa/event/Enhance/EnhanceItemDmgEvent.java
@@ -1,17 +1,16 @@
 package teamzesa.event.Enhance;
 
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
-import teamzesa.entity.User;
 import teamzesa.event.register.EventRegister;
-import teamzesa.util.userHandler.UserController;
+import teamzesa.util.Enum.WeaponMap;
 
 public class EnhanceItemDmgEvent implements EventRegister {
-    private static final float ENHANCE_INCREASE_DAMAGE_PERCENTAGE = 10;
+    private static final double ENHANCE_INCREASE_DAMAGE_PERCENTAGE = 10;
 
-    private Entity targetEntity;
     private Player damager;
     private ItemStack weapon;
     private final EntityDamageEvent event;
@@ -27,10 +26,7 @@ public class EnhanceItemDmgEvent implements EventRegister {
         if (this.event.getDamageSource().getDirectEntity() instanceof Player player) {
             this.damager = player;
             this.weapon = player.getInventory().getItemInMainHand();
-        }
-        else this.event.setCancelled(true);
-
-        this.targetEntity = this.event.getEntity();
+        } else this.event.setCancelled(true);
     }
 
     @Override
@@ -38,15 +34,47 @@ public class EnhanceItemDmgEvent implements EventRegister {
         if (this.damager == null)
             return;
 
-//        debug
-//        float currentWeaponDamage = this.weapon.
-//        float attackDamage = attackDamageCalculator();
+        if (!this.weapon.hasCustomModelData())
+            return;
 
+        double baseDamage = getBaseDamage();
+        double enchantmentDamage = calculateEnchantmentDamage();
+        double modelDataDamage = calculateModelDataDamage();
+        double totalDamage = baseDamage + enchantmentDamage + modelDataDamage;
+
+        this.event.setDamage(totalDamage);
     }
 
-    public float attackDamageCalculator() {
-        float cnt = Float.parseFloat(String.valueOf(this.weapon.getCustomModelData()));
-        System.out.println("cnt > " + cnt);
-        return ENHANCE_INCREASE_DAMAGE_PERCENTAGE + cnt;
+    private double getBaseDamage() {
+        for (WeaponMap weaponInfo : WeaponMap.values()) {
+            if (weaponInfo.getMaterial().equals(this.weapon.getType()))
+                return weaponInfo.getDamage();
+        }
+        return 0;
     }
+
+    private double calculateEnchantmentDamage() {
+        int enchantLevel = this.weapon.getEnchantLevel(Enchantment.DAMAGE_ALL);
+        double enchantmentDamage = 0;
+        for (int i = 0; i < enchantLevel; i++) {
+            switch (i) {
+                case 0 -> enchantmentDamage += 1;
+                case 1, 2, 3, 4 -> enchantmentDamage += 0.5;
+            }
+        }
+        return enchantmentDamage;
+    }
+
+    private double calculateModelDataDamage() {
+        double damage = getBaseDamage();
+        double modelData = this.weapon.getCustomModelData();
+        double modelDataDamage = 0;
+
+        for (int i = 1; i < modelData; i++) {
+            double increaseDmg = damage * (ENHANCE_INCREASE_DAMAGE_PERCENTAGE + i) / 100;
+            modelDataDamage += increaseDmg;
+        }
+        return modelDataDamage;
+    }
+
 }
