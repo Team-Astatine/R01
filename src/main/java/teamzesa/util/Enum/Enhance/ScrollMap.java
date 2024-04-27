ff6e4820c180847c9a2c5762f2c691997dbe9185
JAXPLE <jaxple@gmail.com>
2024-04-27 22:39:33 +0900

    Add new Scroll Sculk / EnderPearl

--- src/main/java/teamzesa/util/Enum/Enhance/ScrollMap.java
+++ src/main/java/teamzesa/util/Enum/Enhance/ScrollMap.java
@@ -7,12 +7,15 @@ public enum ScrollMap {
     END_CRYSTAL(Material.END_CRYSTAL, 1,1),
     ENCHANTED_GOLDEN_APPLE(Material.ENCHANTED_GOLDEN_APPLE, 1,1),
     NETHER_STAR(Material.NETHER_STAR, 1,1),
+    SCULK_SHRIEKER(Material.SCULK_SHRIEKER, 2,4),
+    SCULK_SENSOR(Material.SCULK_SENSOR, 1,3),
     TNT(Material.TNT, 1,2),
     CONDUIT(Material.CONDUIT, 1,2),
     DRAGON_HEAD(Material.DRAGON_HEAD, 1,2),
     RABBIT_FOOT(Material.RABBIT_FOOT, 2,3),
     HEART_OF_THE_SEA(Material.HEART_OF_THE_SEA, 3,6),
-    TOTEM_OF_UNDYING(Material.TOTEM_OF_UNDYING, 8,16);
+    TOTEM_OF_UNDYING(Material.TOTEM_OF_UNDYING, 8,16),
+    ENDER_PEARL(Material.ENDER_PEARL, 16,16);
 
     private final Material material;
     private final int discountValue;
