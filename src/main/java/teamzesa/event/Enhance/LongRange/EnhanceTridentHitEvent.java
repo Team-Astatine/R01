a8564409d59221affa1f5a627ab900f541534acd
JAXPLE <jaxple@gmail.com>
2024-05-03 23:29:05 +0900

    Refactoring Logic

--- src/main/java/teamzesa/event/Enhance/LongRange/EnhanceTridentHitEvent.java
+++ src/main/java/teamzesa/event/Enhance/LongRange/EnhanceTridentHitEvent.java
@@ -12,25 +12,12 @@ import teamzesa.event.Enhance.EnhanceUtil;
 import teamzesa.event.EventRegister.EventRegister;
 
 public class EnhanceTridentHitEvent extends EnhanceUtil implements EventRegister {
-    private Player player;
-    private Trident trident;
-    private ItemStack playerHandTrident;
     private Location tridentHitLocation;
     private final ProjectileHitEvent event;
 
     public EnhanceTridentHitEvent(ProjectileHitEvent event) {
         this.event = event;
-
-        if (!(this.event.getEntity() instanceof Trident trident))
-            return;
-
-        if (!(this.event.getEntity().getShooter() instanceof Player player))
-            return;
-
-        this.player = player;
-        this.trident = trident;
-        this.playerHandTrident = this.player.getInventory().getItemInMainHand();
-
+        init();
         execute();
     }
 
@@ -39,8 +26,16 @@ public class EnhanceTridentHitEvent extends EnhanceUtil implements EventRegister
 
     @Override
     public void execute() {
-        this.tridentHitLocation = this.trident.getLocation();
-        switch (getItemCustomModelData(this.playerHandTrident)) {
+        if (!(this.event.getEntity() instanceof Trident trident))
+            return;
+
+        if (!(this.event.getEntity().getShooter() instanceof Player player))
+            return;
+
+        this.tridentHitLocation = trident.getLocation();
+        ItemStack mainHandTrident = player.getInventory().getItemInMainHand();
+
+        switch (getItemCustomModelData(mainHandTrident)) {
             case 1,2,3 -> executeEnhanceState(Sound.ENTITY_GHAST_DEATH, 0, false);
             case 4,5,6 -> executeEnhanceState(Sound.ENTITY_ENDER_DRAGON_HURT, 1F, false);
             case 7,8,9 -> executeEnhanceState(Sound.ENTITY_ENDER_DRAGON_AMBIENT, 3F, false);
--- src/main/java/teamzesa/event/Enhance/LongRange/EnhanceTridentShotEvent.java
+++ src/main/java/teamzesa/event/Enhance/LongRange/EnhanceTridentShotEvent.java
@@ -9,36 +9,31 @@ import teamzesa.event.Enhance.EnhanceUtil;
 import teamzesa.event.EventRegister.EventRegister;
 
 public class EnhanceTridentShotEvent extends EnhanceUtil implements EventRegister {
-    private Player player;
     private Trident trident;
-    private ItemStack playerHandTrident;
     private final ProjectileLaunchEvent event;
 
     public EnhanceTridentShotEvent(ProjectileLaunchEvent event) {
         this.event = event;
+        init();
+        execute();
+    }
 
+    @Override
+    public void init() {}
+
+    @Override
+    public void execute() {
         if (!(this.event.getEntity() instanceof Trident trident))
             return;
 
         if (!(this.event.getEntity().getShooter() instanceof Player player))
             return;
 
-        this.player = player;
         this.trident = trident;
-        this.playerHandTrident = this.player.getInventory().getItemInMainHand();
+        ItemStack mainHandTrident = player.getInventory().getItemInMainHand();
 
-        execute();
-    }
-
-    @Override
-    public void init() {
-
-    }
-
-    @Override
-    public void execute() {
         Vector vector = this.event.getEntity().getVelocity();
-        switch (getItemCustomModelData(this.playerHandTrident)) {
+        switch (getItemCustomModelData(mainHandTrident)) {
             case 1,2,3 -> executeEnhanceState(1,1, 1, Sound.ENTITY_GHAST_DEATH, vector);
             case 4,5,6 -> executeEnhanceState(1,2, 3, Sound.ENTITY_ENDER_DRAGON_HURT, vector);
             case 7,8,9 -> executeEnhanceState(2,3, 6, Sound.ENTITY_ENDER_DRAGON_AMBIENT, vector);
