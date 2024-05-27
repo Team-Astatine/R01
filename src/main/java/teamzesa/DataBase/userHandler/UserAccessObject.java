9becd78d36b7d97e6d638cff479de68492c14b1e
JAXPLE <jaxple@gmail.com>
2024-05-27 23:36:50 +0900

    Add Refactoring DataBase Class

---
+++ src/main/java/teamzesa/DataBase/userHandler/UserAccessObject.java
@@ -0,0 +1,11 @@
+package teamzesa.DataBase.userHandler;
+
+import teamzesa.DataBase.DataBase;
+import teamzesa.entity.User;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class UserAccessObject {
+
+}
