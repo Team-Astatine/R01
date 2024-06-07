9af24cf94a631e2dac4db39412e28ec38b20d6d7
JwonLee <114869036+JAXPLE@users.noreply.github.com>
2024-06-07 15:46:08 +0900

    Add UserKillStatus Value Checker

--- src/main/java/teamzesa/command/ValueObjectChecker.java
+++ src/main/java/teamzesa/command/ValueObjectChecker.java
@@ -6,29 +6,35 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import teamzesa.DataBase.UserKillStatusHandler.KillStatusController;
+import teamzesa.DataBase.entity.UserKillStatus;
 import teamzesa.util.Interface.StringComponentExchanger;
 import teamzesa.util.Enum.ColorMap;
 import teamzesa.DataBase.entity.User;
 import teamzesa.DataBase.userHandler.UserController;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public class ValueObjectChecker extends StringComponentExchanger implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        Optional.ofNullable(new UserController().readUser(args[0]))
+        Player player = Bukkit.getPlayer(args[0]);
+        Optional.ofNullable(player)
                 .ifPresentOrElse(
-                    existUser -> sendComment(sender, existUser),
+                    existPlayer -> sendComment(sender, existPlayer),
                     () ->        sendComment(sender, null)
                 );
 
         return true;
     }
 
-    private void sendComment(CommandSender sender, User user) {
-        String userInfo = user == null ? "존재하지 않는 유저" : user.toString();
-        if (sender instanceof Player player)
-            playerSendMsgComponentExchanger(player, userInfo, ColorMap.YELLOW);
-        else Bukkit.getLogger().info(user.toString());
+    private void sendComment(CommandSender sender, Player player) {
+        User user = new UserController().readUser(player.getUniqueId());
+        UserKillStatus userKillStatus = new KillStatusController().readUser(player.getUniqueId());
+
+        if (sender instanceof Player)
+            playerSendMsgComponentExchanger(player, user + " " + userKillStatus, ColorMap.YELLOW);
+        else Bukkit.getLogger().info(user + " " + userKillStatus);
     }
 }
