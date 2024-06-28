9ea544955bc8fb59332cb43ddaf8512b7ad5db1e
JAXPLE <jaxple@gmail.com>
2024-06-28 23:16:25 +0900

    Add RestrictedElement Control Class

---
+++ src/main/java/teamzesa/event/Restricted/RestrictedElement.java
@@ -0,0 +1,19 @@
+package teamzesa.event.Restricted;
+
+import org.bukkit.Material;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RestrictedElement {
+    public final Set<Material> restrictedItem = new HashSet<>();
+    public final Set<String> restrictedCommand = new HashSet<>();
+
+    public RestrictedElement() {
+        restrictedItem.add(Material.ARMOR_STAND);
+
+        restrictedCommand.add("/pl");
+        restrictedCommand.add("/plugins");
+    }
+}
