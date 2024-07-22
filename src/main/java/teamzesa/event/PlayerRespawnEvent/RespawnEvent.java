199948ebac4496bda02d2df363c816f0ada75bb3
JAXPLE <jaxple@gmail.com>
2024-07-23 06:23:21 +0900

    Remove comment isBed == null

--- src/main/java/teamzesa/event/PlayerRespawnEvent/RespawnEvent.java
+++ src/main/java/teamzesa/event/PlayerRespawnEvent/RespawnEvent.java
@@ -29,7 +29,8 @@ public class RespawnEvent extends StringComponentExchanger implements EventRegis
 
     public void deathRandomTeleport() {
         if (this.player.getPotentialBedLocation() == null)
-            playerSendMsgComponentExchanger(this.player, "침대가 없어 랜덤 텔레포트 되었습니다", ColorMap.ORANGE);
-        else playerSendMsgComponentExchanger(this.player, "침대로 텔레포트 됩니다", ColorMap.YELLOW);
+            return;
+
+        playerSendMsgComponentExchanger(this.player, "침대로 텔레포트 됩니다", ColorMap.YELLOW);
     }
 }
