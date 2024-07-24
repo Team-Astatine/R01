7ef98583db3c494e62211bf58c7b17d3cebb6bce
JAXPLE <jaxple@gmail.com>
2024-07-24 22:41:24 +0900

    Refactoring Korean, English Exchanger Change Default RandomTeleport World -> Not Player World

--- src/main/java/teamzesa/event/PlayerJoinEvent/ImportPlayerStatusEvent.java
+++ src/main/java/teamzesa/event/PlayerJoinEvent/ImportPlayerStatusEvent.java
@@ -1,5 +1,6 @@
 package teamzesa.event.PlayerJoinEvent;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -13,6 +14,7 @@ import teamzesa.DataBase.entity.RObject.UserKillStatus;
 import teamzesa.command.God;
 import teamzesa.DataBase.entity.RObject.User;
 import teamzesa.event.EventRegister.EventRegister;
+import teamzesa.util.Enum.WorldName;
 import teamzesa.util.Interface.StringComponentExchanger;
 import teamzesa.util.Enum.Kit.ArmourKit;
 import teamzesa.util.Enum.ColorMap;
@@ -64,8 +66,9 @@ public class ImportPlayerStatusEvent extends StringComponentExchanger implements
         if (this.player.hasPlayedBefore())
             return;
 
-        int[] position = RanNumGenerator.groundChecker(this.player.getWorld());
-        World world = this.player.getWorld();
+        World world = Bukkit.getWorld(WorldName.world.getExchangeEnglish());
+        int[] position = RanNumGenerator.groundChecker(world);
+
         int x = position[0];
         int y = position[1];
         int z = position[2];
--- src/main/java/teamzesa/event/PlayerRespawnEvent/RespawnRandomTeleportEvent.java
+++ src/main/java/teamzesa/event/PlayerRespawnEvent/RespawnRandomTeleportEvent.java
@@ -1,10 +1,12 @@
 package teamzesa.event.PlayerRespawnEvent;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import teamzesa.event.EventRegister.EventRegister;
+import teamzesa.util.Enum.WorldName;
 import teamzesa.util.Interface.StringComponentExchanger;
 import teamzesa.util.RanNumGenerator;
 
@@ -27,8 +29,12 @@ public class RespawnRandomTeleportEvent extends StringComponentExchanger impleme
 
     @Override
     public void execute() {
-        int[] position = RanNumGenerator.groundChecker(this.player.getWorld());
-        World world = this.player.getWorld();
+        if (this.player.getPotentialBedLocation() != null)
+            return;
+
+        World world = Bukkit.getWorld(WorldName.world.getExchangeEnglish());
+        int[] position = RanNumGenerator.groundChecker(world);
+
         int x = position[0];
         int y = position[1];
         int z = position[2];
