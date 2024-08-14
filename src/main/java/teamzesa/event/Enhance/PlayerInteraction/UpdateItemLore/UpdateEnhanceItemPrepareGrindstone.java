303290f975617bbfd13c5be27fbf955be0d9cf85
JAXPLE <jaxple@gmail.com>
2024-08-14 10:16:14 +0900

    Add: PrePare GrindStone Result Item Event

---
+++ src/main/java/teamzesa/event/Enhance/PlayerInteraction/UpdateItemLore/UpdateEnhanceItemPrepareGrindstone.java
@@ -0,0 +1,54 @@
+package teamzesa.event.Enhance.PlayerInteraction.UpdateItemLore;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.inventory.PrepareGrindstoneEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import teamzesa.event.Enhance.Interface.EnhanceUtil;
+import teamzesa.event.EventRegister.EventRegister;
+
+public class UpdateEnhanceItemPrepareGrindstone implements EventRegister {
+
+    private ItemStack resultItem;
+
+    private final PrepareGrindstoneEvent event;
+
+    public UpdateEnhanceItemPrepareGrindstone(PrepareGrindstoneEvent event) {
+        this.event = event;
+
+        init();
+        execute();
+    }
+
+    @Override
+    public void init() {
+        this.resultItem = this.event.getResult();
+    }
+
+    @Override
+    public void execute() {
+        if (this.resultItem == null)
+            return;
+
+        if (BooleanUtils.isFalse(this.resultItem.hasItemMeta()))
+            return;
+
+        if (BooleanUtils.isFalse(this.resultItem.getItemMeta().hasCustomModelData()))
+            return;
+
+        int enhanceLevel = this.resultItem.getItemMeta().getCustomModelData();
+
+        ItemMeta itemMeta = this.resultItem.getItemMeta();
+        itemMeta.setCustomModelData(0);
+        this.resultItem.setItemMeta(itemMeta);
+
+        try {
+            EnhanceUtil.increaseDmgAndAddLore(this.resultItem, enhanceLevel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.event.setResult(this.resultItem);
+    }
+}
