b7ffd629210e4e5732d164a2ae94d8b01964d992
JAXPLE <jaxple@gmail.com>
2024-08-16 15:18:09 +0900

    Refactoring: if -> Switch Change Conditional State

--- src/main/java/teamzesa/event/PlayerJoinEvent/PlayerFlyEnableEvent.java
+++ src/main/java/teamzesa/event/PlayerJoinEvent/PlayerFlyEnableEvent.java
@@ -24,15 +24,25 @@ public class PlayerFlyEnableEvent extends StringComponentExchanger implements Ev
 
     @Override
     public void init() {
-        if (event instanceof PlayerJoinEvent joinEvent) {
-            this.player = joinEvent.getPlayer();
-            this.comment = "서버에 접속하여 플라이가 활성화 됩니다";
-        } else if (event instanceof PlayerRespawnEvent respawnEvent) {
-            this.player = respawnEvent.getPlayer();
-            this.comment = "리스폰하여 플라이가 재활성화 됩니다";
-        } else if (event instanceof PlayerChangedWorldEvent worldChangeEvent) {
-            this.player = worldChangeEvent.getPlayer();
-            this.comment = "월드를 이동하여 플라이가 재활성화 됩니다";
+        switch (event) {
+            case PlayerJoinEvent e -> {
+                this.player = e.getPlayer();
+                this.comment = "서버에 접속하여 플라이가 활성화 됩니다";
+            }
+
+            case PlayerRespawnEvent e -> {
+                if (e.getRespawnReason().equals(PlayerRespawnEvent.RespawnReason.END_PORTAL))
+                    return;
+                this.player = e.getPlayer();
+                this.comment = "리스폰하여 플라이가 재활성화 됩니다";
+            }
+
+            case PlayerChangedWorldEvent e -> {
+                this.player = e.getPlayer();
+                this.comment = "월드를 이동하여 플라이가 재활성화 됩니다";
+            }
+
+            default -> throw new IllegalStateException("Unexpected Event value: " + event);
         }
     }
 
