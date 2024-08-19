ff4600c7701f3493804677a05fd2bae094f7628c
JAXPLE <jaxple@gmail.com>
2024-08-20 01:50:01 +0900

    Remove: Miss Typing

--- src/main/java/teamzesa/event/Enhance/PlayerInteraction/UpdateItemLore/UpdateEnhanceItemLoreFromEnchantment.java
+++ src/main/java/teamzesa/event/Enhance/PlayerInteraction/UpdateItemLore/UpdateEnhanceItemLoreFromEnchantment.java
@@ -55,7 +55,7 @@ public class UpdateEnhanceItemLoreFromEnchantment extends StringComponentExchang
         this.enchantItem.setItemMeta(targetItemMeta);
 
         try {
-w            EnhanceUtil.updateEnhanceItemLore(this.enchantItem, enhanceLevel);
+            EnhanceUtil.updateEnhanceItemLore(this.enchantItem, enhanceLevel);
         } catch (Exception e) {
             e.printStackTrace();
         }
