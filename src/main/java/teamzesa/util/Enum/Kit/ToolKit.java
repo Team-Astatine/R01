4edab3baccc53ab12022cc1eb5c3c6b23d4303b7
JAXPLE <jaxple@gmail.com>
2024-08-22 01:50:34 +0900

    Edit: Change iron_ingot amount

--- src/main/java/teamzesa/util/Enum/Kit/ToolKit.java
+++ src/main/java/teamzesa/util/Enum/Kit/ToolKit.java
@@ -6,7 +6,7 @@ import org.bukkit.inventory.ItemStack;
 public enum ToolKit {
     IRON_AXE(new ItemStack(Material.IRON_AXE), 1),
     SHIELD(new ItemStack(Material.SHIELD), 1),
-    IRON_INGOT(new ItemStack(Material.IRON_INGOT), 1);
+    IRON_INGOT(new ItemStack(Material.IRON_INGOT), 16);
 
     private final ItemStack toolKit;
     private final int itemCount;
