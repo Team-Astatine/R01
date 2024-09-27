8c09c33633658bfc1826ace16df756048d5e0631
JwonLee <114869036+JAXPLE@users.noreply.github.com>
2024-09-27 17:02:42 +0900

    Edit: Extend EnhanceItem

--- src/main/java/teamzesa/DataBase/enhance/Armour.java
+++ src/main/java/teamzesa/DataBase/enhance/Armour.java
@@ -2,7 +2,7 @@ package teamzesa.DataBase.enhance;
 
 import org.bukkit.Material;
 
-public interface Armour {
+public interface Armour extends EnhanceItem {
     static <E extends Armour> E findByMaterial(Material material) throws Exception {
         return null;
     }
