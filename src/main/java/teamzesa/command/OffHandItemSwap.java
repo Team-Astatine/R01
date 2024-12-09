6c8c666dcdc34eee6a20a729cdc78356f115b957
JAXPLE <jaxple@gmail.com>
2024-12-09 22:53:20 +0900

    Add '/swap' Command

---
+++ src/main/java/teamzesa/command/OffHandItemSwap.java
@@ -0,0 +1,33 @@
+package teamzesa.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import teamzesa.command.register.CommandRegisterSection;
+import teamzesa.util.Enum.CommandType;
+
+public class OffHandItemSwap extends CommandRegisterSection {
+
+    public OffHandItemSwap() {
+        super(CommandType.OFF_HAND_ITEM_SWAP_FOR_BE);
+    }
+
+    @Override
+    public boolean onCommand(final @NotNull CommandSender commandSender,
+                             final @NotNull Command command,
+                             final @NotNull String s,
+                             final @NotNull String[] strings) {
+
+        Player player = (Player) commandSender;
+
+        ItemStack mainHandItem = player.getInventory().getItemInMainHand();
+        ItemStack offHandItem = player.getInventory().getItemInOffHand();
+
+        player.getInventory().setItemInMainHand(offHandItem);
+        player.getInventory().setItemInOffHand(mainHandItem);
+
+        return true;
+    }
+}
