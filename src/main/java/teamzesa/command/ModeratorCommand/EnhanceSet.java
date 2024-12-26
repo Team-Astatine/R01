08ba64a78fd0932bbc2d829d0bf0969a94f8eb2b
JAXPLE <jaxple@gmail.com>
2024-12-26 22:34:27 +0900

    Edit: 10강에서 하락 시 아이템 불타는 속성 값 적용작업. .setDamageResistant(Tag<DamageType>) Func 더 확인해 봐야함.

--- src/main/java/teamzesa/command/ModeratorCommand/EnhanceSet.java
+++ src/main/java/teamzesa/command/ModeratorCommand/EnhanceSet.java
@@ -5,6 +5,7 @@ import org.bukkit.Tag;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.damage.DamageType;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -15,6 +16,8 @@ import teamzesa.command.register.CommandRegisterSection;
 import teamzesa.event.Enhance.EnhanceUtil;
 import teamzesa.Enum.ColorList;
 import teamzesa.Enum.CommandType;
+
+import java.util.Map;
 
 import static org.yaml.snakeyaml.tokens.Token.ID.Tag;
 
@@ -48,12 +51,18 @@ public class EnhanceSet extends CommandRegisterSection {
         EnhanceItemUndoInformation itemInformation = EnhanceItemUndoInformation.findByMaterial(targetItem);
 
         if (enhanceLevel == 0) {
-            player.getInventory().setItemInMainHand(new ItemStack(targetItem.getType(), 1));
+            ItemStack undoItem = new ItemStack(targetItem.getType(), 1);
+            Map<Enchantment, Integer> targetItemEnchant = targetItem.getEnchantments();
+
+            undoItem.addEnchantments(targetItemEnchant);
+            player.getInventory().setItemInMainHand(undoItem);
             return true;
-        } else {
+        }
+
+        else {
             targetItemMeta.setCustomModelData(0);
             targetItemMeta.setUnbreakable(false);
-            targetItemMeta.setFireResistant(itemInformation.isNetherite());
+            targetItemMeta.setDamageResistant(null);
             targetItemMeta.setRarity(itemInformation.getItemRarity());
 
             targetItem.setItemMeta(targetItemMeta);
