876e74eab5d371e33120bba5a9f74fc2320633b6
JwonLEE <jaxple@gmail.com>
2025-05-03 15:20:19 +0900

    Remove Debugging Print

--- src/main/java/teamzesa/event/EntityDeathEvent/BossDeathListener.java
+++ src/main/java/teamzesa/event/EntityDeathEvent/BossDeathListener.java
@@ -24,8 +24,6 @@ public class BossDeathListener extends StringComponentExchanger implements Event
         this.event = event;
 
         this.bossType = BossType.findByEntityType(this.event.getEntityType());
-        System.out.println(this.bossType);
-
         if (bossType == null)
             return;
 
