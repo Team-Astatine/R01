fb8eac372c3885beb9a65e7d27cfb13bc3890427
JwonLEE <jaxple@gmail.com>
2025-05-06 00:14:15 +0900

    Edit: God 모드 적용 플레이어 일 경우 킬 카운트 제외

--- src/main/java/teamzesa/Event/PlayerDeathEvent/UpdateKillStatusForKiller.java
+++ src/main/java/teamzesa/Event/PlayerDeathEvent/UpdateKillStatusForKiller.java
@@ -3,6 +3,7 @@ package teamzesa.Event.PlayerDeathEvent;
 import org.apache.commons.lang3.ObjectUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import teamzesa.Data.User.UserData.UserController;
 import teamzesa.Data.User.UserKillStatus.KillStatusBuilder;
 import teamzesa.Data.User.UserKillStatus.KillStatusController;
 import teamzesa.Data.User.UserKillStatus.UserKillStatus;
@@ -10,6 +11,7 @@ import teamzesa.Event.EventRegister.EventRegister;
 
 public class UpdateKillStatusForKiller implements EventRegister {
     private KillStatusController controller = new KillStatusController();
+    private UserController userController = new UserController();
 
     private UserKillStatus userKillStatus;
     private final PlayerDeathEvent event;
@@ -30,6 +32,9 @@ public class UpdateKillStatusForKiller implements EventRegister {
         Player player = this.event.getPlayer().getKiller();
 
         if (ObjectUtils.isEmpty(player))
+            return;
+
+        if (this.userController.readUser(player.getUniqueId()).godMode())
             return;
 
         this.userKillStatus = this.controller.readUser(player.getUniqueId());
