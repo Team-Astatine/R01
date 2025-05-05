bc9c2cce68f80fbc45645d5e9ab5d9ecfc7b5d55
JwonLEE <jaxple@gmail.com>
2025-05-06 01:10:07 +0900

    Edit: 킬 카운트 설정 피드백 코드 작업

--- src/main/java/teamzesa/command/ModeratorCommand/SetKillCount.java
+++ src/main/java/teamzesa/command/ModeratorCommand/SetKillCount.java
@@ -1,16 +1,27 @@
 package teamzesa.command.ModeratorCommand;
 
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.ObjectUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import teamzesa.Data.User.UserData.User;
+import teamzesa.Data.User.UserData.UserController;
 import teamzesa.Data.User.UserKillStatus.KillStatusBuilder;
 import teamzesa.Data.User.UserKillStatus.KillStatusController;
 import teamzesa.Data.User.UserKillStatus.UserKillStatus;
 import teamzesa.Enumeration.Command.ListOfCommand;
+import teamzesa.Enumeration.Type.ColorType;
 import teamzesa.command.CommandRegisterSection;
 
+import java.util.Optional;
+
 public class SetKillCount extends CommandRegisterSection {
+    private Player senderPlayer;
+    private Player targetPlayer;
+    private boolean consoleSend = false;
 
     public SetKillCount() {
         super(ListOfCommand.KILL_COUNT_SET);
@@ -22,17 +33,53 @@ public class SetKillCount extends CommandRegisterSection {
                              final @NotNull String s,
                              final @NotNull String[] strings) {
 
-        UserKillStatus targetUser = null;
+        User senderUser = null;
         try {
-            targetUser = new KillStatusController().readUser(Bukkit.getPlayer(strings[0]).getUniqueId());
+            senderUser = new UserController().readUser(commandSender.getName());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        new KillStatusBuilder(targetUser)
-                .killCount(Integer.parseInt(strings[1]))
-                .buildAndUpdate();
+        Optional.ofNullable(senderUser).ifPresentOrElse(
+                existUser -> this.senderPlayer = Bukkit.getPlayer(existUser.uuid()),
+                () -> this.consoleSend = true
+        );
 
+        if (ObjectUtils.allNotNull(senderUser) && BooleanUtils.isFalse(this.senderPlayer.isOp())) {
+            playerSendMsgComponentExchanger(this.senderPlayer, "해당 명령어는 플레이어가 사용할 수 없습니다.", ColorType.RED);
+            return false;
+        }
+
+        User targetUser = null;
+        try {
+            targetUser = new UserController().readUser(strings[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int setKillCount = Integer.parseInt(strings[1]);
+        Optional.ofNullable(Bukkit.getPlayer(targetUser.uuid())).ifPresent(
+                player -> {
+                    this.targetPlayer = player;
+                    new KillStatusBuilder(this.targetPlayer)
+                            .killCount(setKillCount)
+                            .buildAndUpdate();
+                }
+        );
+
+        sendComment(setKillCount);
         return true;
+    }
+
+    private void sendComment(int setHealthValue) {
+        String comment = this.targetPlayer.getName() + "님의 킬 카운트가 " + setHealthValue + "으로 설정됐습니다.";
+        if (consoleSend) {
+            Bukkit.getLogger().info("[R01] " + comment);
+            return;
+        }
+
+        if (ObjectUtils.notEqual(this.senderPlayer, this.targetPlayer))
+            playerSendMsgComponentExchanger(this.senderPlayer, comment, ColorType.YELLOW);
+        playerSendMsgComponentExchanger(this.targetPlayer, comment, ColorType.YELLOW);
     }
 }
