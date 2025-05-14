636c0097087231f998c790f39e6754164203a84c
JwonLEE <jaxple@gmail.com>
2025-05-14 22:25:47 +0900

    Refactoring: 10강 삼지창 폭팔 능력상향

--- src/main/java/teamzesa/Event/Enhance/PlayerInteraction/LongRange/Hit/EnhanceTridentHitEvent.java
+++ src/main/java/teamzesa/Event/Enhance/PlayerInteraction/LongRange/Hit/EnhanceTridentHitEvent.java
@@ -54,7 +54,7 @@ public class EnhanceTridentHitEvent implements EventRegister {
             case 1, 2, 3 -> executeEnhanceState(Sound.ENTITY_GHAST_DEATH, 0, false);
             case 4, 5, 6 -> executeEnhanceState(Sound.ENTITY_ENDER_DRAGON_HURT, 1F, false);
             case 7, 8, 9 -> executeEnhanceState(Sound.ENTITY_ENDER_DRAGON_AMBIENT, 3F, false);
-            case 10 -> executeEnhanceState(Sound.ENTITY_WITHER_SPAWN, 6F, true);
+            case 10 -> tridentExplosive(Sound.ENTITY_WITHER_SPAWN, 10F, true);
             default -> {
             }
         }
@@ -62,6 +62,16 @@ public class EnhanceTridentHitEvent implements EventRegister {
 
     private void executeEnhanceState(Sound sound, float power, boolean isFire) {
         Runnable tridentHitTask = () -> {
+            this.tridentHitLocation.getWorld().playSound(this.tridentHitLocation, sound, 5F, 5F);
+            this.tridentHitLocation.getWorld().createExplosion(this.tridentHitLocation, power, isFire);
+        };
+
+        tridentHitTask.run();
+    }
+
+    private void tridentExplosive(Sound sound, float power, boolean isFire) {
+        Runnable tridentHitTask = () -> {
+            this.tridentHitLocation.getWorld().strikeLightning(this.tridentHitLocation);
             this.tridentHitLocation.getWorld().playSound(this.tridentHitLocation, sound, 5F, 5F);
             this.tridentHitLocation.getWorld().createExplosion(this.tridentHitLocation, power, isFire);
         };
